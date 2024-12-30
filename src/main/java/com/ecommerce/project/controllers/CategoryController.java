ad40cef72a18af3f12072dd46776c5270da037f8
sachin-source <sachinmgvt@gmail.com>
2024-12-30 19:37:34 +0530

    get all categories

--- src/main/java/com/ecommerce/project/controllers/CategoryController.java
+++ src/main/java/com/ecommerce/project/controllers/CategoryController.java
@@ -1,4 +1,18 @@
 package com.ecommerce.project.controllers;
 
+import com.ecommerce.project.models.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CategoryController {
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories () {
+        return categories;
+    }
 }
