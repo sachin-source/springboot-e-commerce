fdaeae2f91e16799c4bd9c73f29b45c85399cc45
sachin-source <sachinmgvt@gmail.com>
2024-12-31 15:21:54 +0530

    Mark with service annotation

--- src/main/java/com/ecommerce/project/services/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/services/CategoryServiceImpl.java
@@ -1,17 +1,19 @@
 package com.ecommerce.project.services;
 
 import com.ecommerce.project.models.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<>();
 
     @Override
     public List<Category> getAllCategories() {
-        return List.of();
+        return categories;
     }
 
     @Override
