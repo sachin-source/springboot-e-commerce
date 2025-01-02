936ee1fff8d5529193ce8973a8f2b86fcff75b71
sachin-source <sachinmgvt@gmail.com>
2025-01-02 20:04:05 +0530

    Auto increment categoryId

--- src/main/java/com/ecommerce/project/services/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/services/CategoryServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -18,6 +19,7 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
 }
