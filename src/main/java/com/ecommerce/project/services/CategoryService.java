b52740d06efe75d78e6a7d4df123c13b7358ee9f
sachin-source <sachinmgvt@gmail.com>
2025-01-02 20:14:52 +0530

    delete category method

--- src/main/java/com/ecommerce/project/services/CategoryService.java
+++ src/main/java/com/ecommerce/project/services/CategoryService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface CategoryService {
     List<Category> getAllCategories();
     void createCategory(Category category);
+    void deleteCategory(Long categoryId);
 }
