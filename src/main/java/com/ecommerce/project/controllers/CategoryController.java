f53a54f8a8f86eb77aeeb4d56bf4ea2c0db7ea61
sachin-source <sachinmgvt@gmail.com>
2025-01-02 20:17:02 +0530

    Controller for delete category

--- src/main/java/com/ecommerce/project/controllers/CategoryController.java
+++ src/main/java/com/ecommerce/project/controllers/CategoryController.java
@@ -3,10 +3,7 @@ package com.ecommerce.project.controllers;
 import com.ecommerce.project.models.Category;
 import com.ecommerce.project.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         this.categoryService.createCategory(category);
         return "Category added successfully!";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
