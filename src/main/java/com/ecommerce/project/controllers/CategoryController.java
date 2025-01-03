de08d04720c21dbbd1d96acc8aa46129857601c5
sachin-source <sachinmgvt@gmail.com>
2025-01-03 10:14:55 +0530

    Handle status code for complete API

--- src/main/java/com/ecommerce/project/controllers/CategoryController.java
+++ src/main/java/com/ecommerce/project/controllers/CategoryController.java
@@ -3,7 +3,11 @@ package com.ecommerce.project.controllers;
 import com.ecommerce.project.models.Category;
 import com.ecommerce.project.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -29,8 +33,12 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId) {
-        String status = categoryService.deleteCategory(categoryId);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 }
