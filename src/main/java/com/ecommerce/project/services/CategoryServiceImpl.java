6a0de157e4738513fc9a5ccec270187a05e1fbed
sachin-source <sachinmgvt@gmail.com>
2025-01-03 10:14:18 +0530

    Handle error with status code

--- src/main/java/com/ecommerce/project/services/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/services/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.services;
 
 import com.ecommerce.project.models.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +29,7 @@ public class CategoryServiceImpl implements CategoryService{
     public String deleteCategory(Long categoryId){
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-
-        if(category == null)
-            return "Category not found";
+                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         categories.remove(category);
         return "Category with categoryId " + categoryId + " deleted successfully";
